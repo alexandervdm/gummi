0ce79f52525d664278f567b80cbc43c6a08852cf
Alexander van der Meij <alexandervdm@gliese.me>
2010-09-18 11:32:55 +0000

    temp remove func calls for QA tool

--- src/gui.c
+++ src/gui.c
@@ -588,12 +588,13 @@ void on_button_template_remove_clicked(GtkWidget* widget, void* user) {
 void on_button_template_open_clicked(GtkWidget* widget, void* user) {
     L_F_DEBUG;
     gchar *text;
+    /*
     text = template_open_selected(gummi->templ);
     if (text) {
         editor_fill_buffer(gummi->editor, text);
         gummi_create_environment(gummi, NULL);
         gtk_widget_hide(GTK_WIDGET(gummi->templ->templatewindow));
-    }
+    }*/
 }
 
 void on_button_template_close_clicked(GtkWidget* widget, void* user) {
