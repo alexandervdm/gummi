b83fbfb98b0d25f06a435929758181617849a9c7
Alexander van der Meij <alexandervdm@gliese.me>
2011-10-01 11:13:14 +0000

    updating dev build tools

--- dev/chver.sh
+++ dev/chver.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-sed -i "s/AC_INIT(\([^,]*\), \[\([a-zA-Z0-9.~\-]*\)*\], \(.*$\)/AC_INIT(\1, [$1], \3/g" configure.ac
+if [ "$1" == "" ]; then
+	echo "This script requires a command line argument <version number>"
+fi
 
-autoreconf
+if [ -e "README" ]; then
+	sed -i "s/AC_INIT(\([^,]*\), \[\([a-zA-Z0-9.~\-]*\)*\], \(.*$\)/AC_INIT(\1, [$1], \3/g" configure.ac
+	autoreconf
+else
+	echo "This script should be run from the trunk root directory."
+fi
+
+
