338985ae314e12f0ef53bdd91513107b119c40f2
AZ Huang <aitjcize@gmail.com>
2011-10-01 11:23:54 +0000

    Update dev scripts, now scripts can be run from anywhere.

--- dev/chver.sh
+++ dev/chver.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-	echo "This script requires a command line argument <version number>"
+    echo "This script requires a command line argument <version number>"
 fi
 
-if [ -e "README" ]; then
-	sed -i "s/AC_INIT(\([^,]*\), \[\([a-zA-Z0-9.~\-]*\)*\], \(.*$\)/AC_INIT(\1, [$1], \3/g" configure.ac
-	autoreconf
-else
-	echo "This script should be run from the trunk root directory."
-fi
+absolute_dir=$(cd "$( dirname "$0" )" && pwd)
+cd $absolute_dir/..
+
+sed -i "s/AC_INIT(\([^,]*\), \[\([a-zA-Z0-9.~\-]*\)*\], \(.*$\)/AC_INIT(\1, [$1], \3/g" configure.ac
+autoreconf
 
 
